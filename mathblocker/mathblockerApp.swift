3483712e7fc947680c0eb5b9f5a4ab922f58a7ff
Andy Phu <andy.jiapao.phu@gmail.com>
2026-04-09 02:15:51 -0700

    Initial Commit

---
+++ mathblocker/mathblockerApp.swift
@@ -0,0 +1,32 @@
+//
+//  mathblockerApp.swift
+//  mathblocker
+//
+//  Created by Andy Phu on 4/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct mathblockerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ mathblockerTests/mathblockerTests.swift
@@ -0,0 +1,19 @@
+//
+//  mathblockerTests.swift
+//  mathblockerTests
+//
+//  Created by Andy Phu on 4/9/26.
+//
+
+import Testing
+@testable import mathblocker
+
+struct mathblockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
