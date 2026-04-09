13342d508b83f29de7d7eb7dfefe77deaf8f85a2
Andy Phu <andy.jiapao.phu@gmail.com>
2026-04-09 04:10:21 -0700

    feat: add entitlement spec

---
+++ dam/DeviceActivityMonitorExtension.swift
@@ -0,0 +1,12 @@
+//
+//  DeviceActivityMonitorExtension.swift
+//  dam
+//
+//  Created by Andy Phu on 4/9/26.
+//
+
+import DeviceActivity
+
+class DeviceActivityMonitorExtension: DeviceActivityMonitor {
+    // Stub — dame is the real extension
+}
